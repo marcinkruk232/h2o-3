c990dffe80126f67fc43c3ec7c35278d7ce3e62d
Adam Valenta <adam.valenta@h2o.ai>
2021-07-20 02:33:40 +0200

    DONT-COMMIT: install causalml directly from sources

--- scripts/jenkins/groovy/makeTarget.groovy
+++ scripts/jenkins/groovy/makeTarget.groovy
@@ -45,7 +45,12 @@ def call(final pipelineContext, final Closure body) {
         . /envs/h2o_env_python${env.PYTHON_VERSION}/bin/activate
         if [ \$PYTHON_VERSION != '2.7' ]; then 
           # this will also update numpy 
-          pip install causalml==0.10.0
+          git clone https://github.com/uber/causalml.git
+          cd causalml
+          pip install -r requirements.txt
+          python setup.py build_ext --inplace
+          python setup.py install
+          cd ..
         fi
       fi
       
