c5eb254bb553b79a4974383c739d051608be5b2c
michalkurka <michalk@h2o.ai>
2021-07-20 10:54:46 -0400

    RIP

--- h2o-py/tests/testdir_generic_model/pyunit_generic_model.py
+++ h2o-py/tests/testdir_generic_model/pyunit_generic_model.py
@@ -7,29 +7,49 @@ from tests import pyunit_utils
 
 # Test of MOJO convenience methods
 def generic_blank_constructor():
-    
-    # Train a model
-    airlines = h2o.import_file(path=pyunit_utils.locate("smalldata/testng/airlines_train.csv"))
-    model = H2OGradientBoostingEstimator(ntrees = 1)
-    model.train(x = ["Origin", "Dest"], y = "IsDepDelayed", training_frame=airlines)
-    
-    #Save the previously created model into a temporary file
-    original_model_filename = tempfile.mkdtemp()
-    original_model_filename = model.download_mojo(original_model_filename)
-    
-    # Load the model from the temporary using an empty constructor
-    mojo_model = H2OGenericEstimator()
-    mojo_model.path = original_model_filename
-    mojo_model.train()
-    assert isinstance(mojo_model, H2OGenericEstimator)
+    from h2o.estimators import H2ORandomForestEstimator
 
-    assert mojo_model._model_json["output"]["original_model_identifier"] == "gbm"
-    assert mojo_model._model_json["output"]["original_model_full_name"] == "Gradient Boosting Machine"
+    h2o_df = h2o.import_file("/Users/mkurka/git/h2o/h2o-3/private/datasets/BNPParibas.csv")
 
-    # Test scoring is available on the model
-    predictions = mojo_model.predict(airlines)
-    assert predictions is not None
-    assert predictions.nrows == 24421
+    predictors = h2o_df.columns
+    response = "v58"
+    predictors.remove(response)
+
+    cars_drf = H2ORandomForestEstimator(
+        ntrees=1,
+        max_depth=10,
+        min_rows=1,
+        nbins=4096,
+        nbins_top_level=4096,
+        min_split_improvement=1e-06,
+        seed=3
+    )
+    cars_drf.train(x=predictors,
+                   y=response,
+                   training_frame=h2o_df,
+                   )
+
+    from h2o.tree import H2OTree
+
+    print(cars_drf.predict(h2o_df))
+    node_assignments_num = cars_drf.predict_leaf_node_assignment(h2o_df, type="Node_ID")
+
+    mojo_path = cars_drf.download_mojo("prcak")
+    print(h2o.print_mojo(mojo_path, format="json"))
+    tree = H2OTree(model=cars_drf, tree_number=0)
+    children = []
+    for i in range(0, len(tree)):
+        if (tree.left_children[i] == -1):
+            children.append(tree.node_ids[i])
+
+    print(len(node_assignments_num.unique()))
+    print(len(children))
+     
+    xx = set(node_assignments_num.as_data_frame()["T1"])
+    print(set(children) - xx)
+    
+    assert len(node_assignments_num.unique()) == len(children)
+
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(generic_blank_constructor)
